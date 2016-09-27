32267499d76f1b3038ebf33de635b60dd4a28df1
take <take@3f80588d-1151-ba42-9a6c-a031a1e8ad9a>
2016-09-27 12:01:30 +0000

    FunaUtil作成
    git-svn-id: file:///home/centos/data/source/svn_repos/jEdit_plugin/FunaUtil@954 3f80588d-1151-ba42-9a6c-a031a1e8ad9a

---
+++ trunk/funa/util/FunaUtil.java
@@ -0,0 +1,14 @@
+package funa.util;
+
+public class FunaUtil extends EBPlugin {
+  
+  public FunaUtil(){
+    System.out.println("FunaUtil1");
+  }
+  
+  public void handleMessage( EBMessage msg ) {
+    System.out.println("FunaUtil1");
+    
+  }
+}
+
