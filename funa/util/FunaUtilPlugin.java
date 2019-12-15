5874364dcfda990e9804ccc02402f3204e1b230f
funatake <centos@localhost.localdomain>
2019-12-15 12:15:31 +0900

    ライブラリではなくプラグインにする

---
+++ funa/util/FunaUtilPlugin.java
@@ -0,0 +1,8 @@
+package funa.util;
+import org.gjt.sp.jedit.EBPlugin;
+import org.gjt.sp.jedit.EBMessage;
+
+public class FunaUtilPlugin extends EBPlugin {
+  public void handleMessage( EBMessage msg ) {
+  }
+}
