eab51e0bec65bebee47718396b123b03d73078d4
funatake <centos@localhost.localdomain>
2025-03-21 10:41:47 +0900

    整形時のMarker位置の保存方法を変更
    offsetで記憶していたのを、行数とその行の後ろからの位置で記憶するように修正

--- funa/util/MarkerManager.java
+++ funa/util/MarkerManager.java
@@ -9,9 +9,27 @@ public class MarkerManager {
   static private class Data {
     char shortcut;
     int position;
-    private Data(Marker marker) {
+    int line;
+    int offsetFromEndOfLine;
+    Buffer buffer;
+    private Data(Marker marker, Buffer buffer) {
       this.shortcut = marker.getShortcut();
       this.position = marker.getPosition();
+      this.buffer = buffer;
+      this.line = buffer.getLineOfOffset(this.position);
+      this.offsetFromEndOfLine = buffer.getLineEndOffset(this.line) - this.position - 1;
+    }
+    
+    public int getPosition() {
+      if (buffer.getLineCount() <= this.line) {
+        return buffer.getLength();
+      }
+      int offset = buffer.getLineEndOffset(this.line) - 1;
+      offset = offset - offsetFromEndOfLine;
+      if (offset < buffer.getLineStartOffset(this.line)) {
+        offset = buffer.getLineStartOffset(this.line);
+      }
+      return offset;
     }
   }
   private ArrayList<Data> markers = new ArrayList<Data>();
@@ -20,13 +38,14 @@ public class MarkerManager {
     markers.clear();
     Vector<Marker> bufferMarkers = buffer.getMarkers();
     for(Marker marker: bufferMarkers) {
-      markers.add(new Data(marker));
+      markers.add(new Data(marker, buffer));
     }
   }
   
   public void restore(Buffer buffer) {
     for(Data data: markers) {
-      buffer.addMarker(data.shortcut, data.position);
+      // int position = data.position <= buffer.getLength() ? data.position : buffer.getLength();
+      buffer.addMarker(data.shortcut, data.getPosition());
     }
   }
 }
